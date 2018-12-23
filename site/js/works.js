f91537d93ae3520f8f39077dd133a3b55e92c121
Taishi <hayakt@uw.edu>
2018-12-23 15:20:07 -0800

    portfolio

--- site/js/works.js
+++ site/js/works.js
@@ -18,3 +18,25 @@ $('#icon-contact').mouseover(function(){
 }).mouseleave(function(){
     $(this).attr('src','../../images/contact.png');
 });
+var artworks = [
+    {
+        name:'lemon',
+        src:'../../artworks/lemon.jpg'},
+    {
+        name:'tomato',
+        src:'../../artworks/tomato.jpg'},
+    {
+        name:'seattle',
+        src:'../../artworks/seattle.png'},
+    {   
+        name:'Light in Dark',
+        src:'../../artworks/lightInDark.jpg'},
+    {
+        name:'Shoe',
+        src:'../../artworks/shoe.jpg'}
+];
+var order = [4,3,2,1,0]
+for(i = 0; i < artworks.length; i++){
+    $('.artworks').append('<img class="thumbnail" src="'+ artworks[order[i]].src +'"></img>');
+
+}
