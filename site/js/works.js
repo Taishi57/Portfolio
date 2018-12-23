0c5a045fe33b3cdd38b08e1fb14dd8b2f6daca0d
Taishi <hayakt@uw.edu>
2018-12-23 15:31:46 -0800

    portfolio

--- site/js/works.js
+++ site/js/works.js
@@ -32,10 +32,19 @@ var artworks = [
         name:'Light in Dark',
         src:'../../artworks/lightInDark.jpg'},
     {
+        name:'Desk',
+        src:'../../artworks/desk.jpg'},
+    {
+        name:'pepper',
+        src:'../../artworks/pepper.jpg'},
+    {
         name:'Shoe',
-        src:'../../artworks/shoe.jpg'}
+        src:'../../artworks/shoe.jpg'},
+    {
+        name:'Seesaw',
+        src:'../../artworks/seesaw.gif'}
 ];
-var order = [4,3,2,1,0]
+var order = [1,6,2,0,4,5,3,7]
 for(i = 0; i < artworks.length; i++){
     $('.artworks').append('<img class="thumbnail" src="'+ artworks[order[i]].src +'"></img>');
 
