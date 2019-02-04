9c3f9e6479328d18decacafa3b0c40f02dad3514
Taishi <hayakt@uw.edu>
2019-02-04 11:54:54 -0800

    portfolio

--- site/js/works.js
+++ site/js/works.js
@@ -22,15 +22,15 @@ var artworks = [
     {
         name:'Tomato',
         src:'../../artworks/tomato.jpg',
-        description:'This was drawn with an iPad Pro using digital colored pencil. My though process of this was very simple, I wanted to draw fruits and vegtables realistically, but that is way too cliche, so I decided to crush the fruits. This not only avoids the common fruits drawings, but it adds to the challenge of drawing the juice of the interior. This drawing really tested my observation use of colors to be able to successfully draw the fruit realistically.'},
+        description:'This was drawn with an iPad Pro using digital colored pencil. My though process of this was very simple, I wanted to draw fruits and vegetables realistically, but that is way too cliche, so I decided to crush the fruits. This not only avoids the common fruits drawings, but it adds to the challenge of drawing the juice of the interior. This drawing really tested my observation use of colors to be able to successfully draw the fruit realistically.'},
     {
         name:'Seattle',
         src:'../../artworks/seattle.jpg',
-        description:'I wanted to learn how to use Adobe Illustrator, so I asked some friends if they wanted any lock/home screen made by me. One of my friend requested me to make a lockscreen of seattle, which was exactly what I did. My friend was happy with the results of the locksceen, and I was able to learn more on how to use Illustrator, so it was a Win-Win! Before giving the lockscreen to my friend, I removed some of the raindrops in the middle so that it will be easier to see the displayed time'},
+        description:'I wanted to learn how to use Adobe Illustrator, so I asked some friends if they wanted any lock/home screen made by me. One of my friend requested me to make a lockscreen of seattle, which was exactly what I did. My friend was happy with the results of the locksceen, and I was able to learn more on how to use Illustrator, so it was a Win-Win! Before giving the lock screen to my friend, I removed some of the raindrops in the middle so that it will be easier to see the displayed time.'},
     {   
         name:'Light in Dark',
         src:'../../artworks/lightInDark.jpg',
-        description:'The medium of this drawing is charcoal. I wanted to create a piece that has a large contrast, the inital source of this picture is me in the dark with one bright light source spotted towards me. The hood is for the aesthetic feel. It somewhat represents that I am insecure in the dark, and I am drawn towards the light, possibly meaning freedom or confort.'},
+        description:'The medium of this drawing is charcoal. I wanted to create a piece that has a large contrast, the initial source of this picture is me in the dark with one bright light source spotted towards me. The hood is for the aesthetic feel. It somewhat represents that I am insecure in the dark, and I am drawn towards the light, possibly meaning freedom or confort.'},
     {
         name:'Desk',
         src:'../../artworks/desk.jpg',
@@ -38,7 +38,7 @@ var artworks = [
     {
         name:'Untied',
         src:'../../artworks/shoe.jpg',
-        description:'This piece was very fun to draw, the multiple textures of the shoe such as the leather and the rubber made me use different techniques to be able to accurately represent the surface. This also gave me an oppertunity to try out the different degrees of the pencil (6H-6B) and see how the different hardness affects how my drawing looks.'},
+        description:'This piece was very fun to draw, the multiple textures of the shoe such as the leather and the rubber made me use different techniques to be able to accurately represent the surface. This also gave me an opportunity to try out the different degrees of the pencil (6H-6B) and see how the different hardness affects how my drawing looks.'},
     {
         name:'Totally Realistic Soccer',
         src:'../../artworks/soccer.gif',
