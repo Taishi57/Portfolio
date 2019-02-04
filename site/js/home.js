b13408857da7052ef965157b75e40a05676d91fa
Taishi <hayakt@uw.edu>
2019-02-04 13:00:00 -0800

    portfolio

--- site/js/home.js
+++ site/js/home.js
@@ -31,3 +31,6 @@ $('#door').mouseover(function(){
 function sent(){
     $('#sent h1').css('visibility','visible');
 }
+$('.nav-item').on("click", "a:not([data-toggle])", null, function () {
+    $('.collapse').collapse('hide');
+});
