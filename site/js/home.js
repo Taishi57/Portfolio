95dec311032e79a8f0acb2f161a9753d4c9b37e4
Taishi <hayakt@uw.edu>
2019-03-26 18:43:07 -0700

    portfolio

--- site/js/home.js
+++ site/js/home.js
@@ -3,14 +3,30 @@ $('#submit').mouseover(function(){
 }).mouseleave(function(){
     $(this).css('background','url("../../images/send.png") no-repeat').css('background-size','100%');
 });
+
+//works door
 $('#door').mouseover(function(){
     $(this).attr('src','../../images/doorOpen2.gif');
 }).mouseleave(function(){
     $(this).attr('src','../../images/doorClose2.png');
 });
+
+//email
 function sent(){
     $('#sent h1').css('visibility','visible');
 }
 $('.collapse').on("click", "a:not([data-toggle])", null, function () {
     $('.collapse').collapse('hide');
+});
+
+//contacts
+$('#envelope img').mouseover(function(){
+    $(this).attr('src','../../images/envelope-open.png');
+}).mouseleave(function(){
+    $(this).attr('src','../../images/envelope-close.png');
+});
+$('#comment img').mouseover(function(){
+    $(this).attr('src','../../images/comment-open.png');
+}).mouseleave(function(){
+    $(this).attr('src','../../images/comment-close.png');
 });
