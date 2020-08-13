6900a84e1be3bafe50e3fd0500f0b3f4b6139906
Taiguy <hayakt@uw.edu>
2020-08-13 06:07:22 -0700

    nav bar improvement

--- site/js/navbar.js
+++ site/js/navbar.js
@@ -45,3 +45,20 @@ $('.navItem a').mouseover(function(){
 }).mouseleave(function(){
     $(this).siblings('img').css('display','none');
 })
+
+//hide navbar when scrolling down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < currentScrollPos && $(window).width() > 1020) {
+    document.getElementById("navbar").style.top = "-120px";
+  } else{
+    document.getElementById("navbar").style.top = "0";
+  }
+  prevScrollpos = currentScrollPos;
+}
+$(window).resize(function() {
+    if($(window).width() < 1020){
+        document.getElementById("navbar").style.top = "0";
+    }
+});
